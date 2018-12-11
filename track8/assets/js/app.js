73114e2398fa7ab7138b5bed65eea7e0b2c99f89
Erin McGee <claue312@newschool.edu>
2018-12-10 20:15:32 -0500

    uwuwu

---
+++ track8/assets/js/app.js
@@ -0,0 +1,7 @@
+$('#theDivThatHoldsTheImage_YouCouldMakeItMoreSemanticIfYouWish').hover(
+  function() {
+      $('#btn').fadeIn('slow');
+  },function() {
+      $('#btn').fadeOut('slow');
+  }
+);
